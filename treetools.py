44e4ba205647dc9c9ae84b01229c686a6f70aa3b
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2020-06-17 00:39:36 -0300

    add converter to convert between representations

--- treetools.py
+++ treetools.py
@@ -1,10 +1,11 @@
 import random
 from itertools import compress, filterfalse
+from evol import Individual
 
+from base.customevol import SteinerIndividual
 from graph import Graph, SteinerTreeProblem
 from graph.disjointsets import DisjointSets
 from graph.priorityqueue import PriorityQueue
-
 
 def vertices_from_binary_chromosome(chromosome, terminals, nro_vertices):
 
@@ -87,8 +88,7 @@ def evaluate_treegraph(chromosome, penality):
 
 class Eval:
 
-    def __init__(self,
-                 STPG : SteinerTreeProblem):
+    def __init__(self, STPG : SteinerTreeProblem):
         self.STPG = STPG
 
     def penality(self, k):
@@ -106,3 +106,64 @@ class Eval:
                                    self.STPG.terminals,
                                    self.STPG.nro_nodes,
                                    self.penality)
+
+
+class Converter:
+
+    def __init__(self, STPG : SteinerTreeProblem):
+        self.STPG = STPG
+
+    def treegraph2binary(self, individual : Individual):
+        '''Converts from TreeChromosome to a BinaryChromosome'''
+
+        subgraph = individual.chromosome
+        terminals = set(self.STPG.terminals)
+        nro_vertices = self.STPG.nro_nodes
+
+        genes = ['0'] * nro_vertices # all vertices in the instance problem
+
+        # vertices in the subgraph (partial solution) include terminals and non-terminals
+        for v in subgraph.vertices:
+            genes[v-1] = '1'
+
+        # choosing only the non_terminals positions
+        genes = (gene for node, gene in enumerate(genes, start=1) if node not in terminals)
+
+        return SteinerIndividual(''.join(genes))
+
+    def binary2treegraph(self, individual : Individual):
+        '''Converts from BinaryChromosome to TreeChromosome'''
+
+        GRAPH = self.STPG.graph
+        terminals = self.STPG.terminals
+        nro_vertices = self.STPG.nro_nodes
+        chromosome = individual.chromosome
+
+        queue = PriorityQueue()
+        disjointset = DisjointSets()
+        subgraph = Graph()
+        dones = set()
+
+        # todos os vértices esperados na solução parcial
+        vertices = vertices = vertices_from_binary_chromosome(chromosome, terminals, nro_vertices)
+
+        # adiciona uma aresta se os vértices extremos da aresta
+        # estão contidos no conjunto vertices
+        # mantém essas arestas em uma fila de prioridades para
+        # formar uma MST baseado no algoritmo de Kruskal
+        for v in vertices:
+            dones.add(v)
+            disjointset.make_set(v) # para construir a MST
+            subgraph.add_node(v) # garantir a inserção de um vértice isolado
+            for u in GRAPH.adjacent_to(v):
+                if (u in vertices) and (u not in dones):
+                    weight = GRAPH.weight(v, u)
+                    queue.push(weight, (v, u, weight))
+
+        while queue:
+            v, u, weight = queue.pop()
+            if disjointset.find(v) != disjointset.find(u):
+                subgraph.add_edge(v, u, weight=weight)
+                disjointset.union(v, u)
+
+        return SteinerIndividual(subgraph)
