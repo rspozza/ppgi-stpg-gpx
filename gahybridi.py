5080526508933f0d8e0db1dc8def028b6233c79f
GiliardGodoi <giliardgodoi@alunos.utfpr.edu.br>
2020-06-17 00:45:53 -0300

    defining a hybridi evolution

--- gahybridi.py
+++ gahybridi.py
@@ -9,25 +9,29 @@ from base.customevol import SteinerPopulation as Population
 from base.mutate import flip_onebit
 from base.normalization import normalize
 from base.selector import roullete
-from evaluate import Eval
 from graph import Graph
 from graph.reader import ReaderORLibrary
+from pxsimpliest import SimpliestPX
+from treetools import Converter, Eval
 from util import display, read_problem
 
 PARAMS = {
-    'dataset' : 'example.txt',
+    'dataset' : 'steinb1.txt',
+    'best_known_cost' : 82,
     'population_size' : 100,
-    'n_iterations' : 5_000,
-    'stagnation_interval' : 1_000
+    'n_iterations' : 25,
+    'stagnation_interval' : 1_000,
 }
 
 
-def simulation(params, trial=0, output=None):
+def simulation(params : dict, trial=0, output=None):
 
-    STPG = read_problem("datasets", "ORLibrary", params.dataset)
+    STPG = read_problem("datasets", "ORLibrary", params["dataset"])
 
-    population_size = params.population_size
+    population_size = params["population_size"]
     lenght = STPG.nro_nodes - STPG.nro_terminals
+
+    converter = Converter(STPG)
 
     population = (Population(chromosomes=[random_binary(lenght) for _ in range(population_size)],
                             eval_function=Eval(STPG),
@@ -36,33 +40,36 @@ def simulation(params, trial=0, output=None):
                         .callback(normalize))
 
     binary = (Evolution()
-        .select(selection_func=roullete)
-        .crossover(combiner=crossover_2points)
-        .mutate(mutate_function=flip_onebit, probability=0.2)
-        .evaluate()
-        .callback(normalize)
-        .callback(display, every=100)
-        )
+                .select(selection_func=roullete)
+                .crossover(combiner=crossover_2points)
+                .mutate(mutate_function=flip_onebit, probability=0.2)
+                .evaluate()
+                .callback(normalize)
+                .callback(display, every=100))
 
     treegraph = (Evolution()
-        .select(selection_func=roullete)
-        .crossover(combiner=SimpliestPX(STPG))
-        .mutate(mutate_function=flip_onebit, probability=0.2)
-        .evaluate()
-        .callback(normalize)
-        .callback(display, every=100)
-        )
+                .select(selection_func=roullete)
+                .crossover(combiner=SimpliestPX(STPG)) # .mutate(mutate_function=flip_onebit, probability=0.2)
+                .evaluate()
+                .callback(normalize)
+                .callback(display, every=100))
 
     hybridi = (Evolution()
-           .repeat(binary, n=200)
-           .map(convert2treegraph)
-           .repeat(treegraph, n=200)
-           .map(convert2binary)
-           .evaluate()
-           )
+                .repeat(binary, n=200)
+                .map(converter.binary2treegraph)
+                .evaluate()
+                .repeat(treegraph, n=200)
+                .map(converter.treegraph2binary)
+                .evaluate())
 
-    with IterationLimit(limit=params.n_iterations), \
-        Stagnation(interval=params.stagnation_interval):
-        result = population.evolve(hybridi, n=params.n_iterations)
+    # with IterationLimit(limit=params["n_iterations"]), \
+    #     Stagnation(interval=params["stagnation_interval"]):
+    #     result = population.evolve(hybridi, n=params["n_iterations"])
+
+    result = population.evolve(hybridi, n=params["n_iterations"])
 
     return result
+
+if __name__ == "__main__":
+
+    simulation(PARAMS)
